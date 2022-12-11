964ace5952bd58a9a2107d1b7ea266365b65fd58
崔 明辉 <cuis@vip.qq.com>
2022-12-11 22:55:58 +0800

    chore: update to mpflutter 0.19.0

--- lib/main.dart
+++ lib/main.dart
@@ -27,24 +27,58 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MPScaffold(
       name: 'Template',
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed('/second');
-          },
-          child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.blue,
-            child: Center(
-              child: Text(
-                'Hello, MPFlutter!',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _renderPushNextWidget(context),
+          SizedBox(height: 8),
+          _renderCallMPJSWidget(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _renderPushNextWidget(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).pushNamed('/second');
+      },
+      child: Container(
+        width: 200,
+        height: 100,
+        color: Colors.blue,
+        child: Center(
+          child: Text(
+            'Hello, MPFlutter!',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _renderCallMPJSWidget(BuildContext context) {
+    return GestureDetector(
+      onTap: () async {
+        final result = await MPJS.evalTemplate('foo', ['MPFlutter']);
+        print(result);
+      },
+      child: Container(
+        width: 200,
+        height: 100,
+        color: Colors.pink,
+        child: Center(
+          child: Text(
+            'Hello, MPJS!',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
           ),
         ),
